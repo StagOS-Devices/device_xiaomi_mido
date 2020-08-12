109c2bb2f22672a0d7136eb43a158a0527f03f84
shashank1436 <9945shashank@gmail.com>
2020-08-12 09:38:59 +0200

    mido: set face unlock & bootanimation res value

--- rr_mido.mk
+++ rr_mido.mk
@@ -23,6 +23,8 @@ $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common RR stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
